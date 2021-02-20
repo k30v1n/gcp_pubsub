0b4232c67878a394f297a3049cc2d81086d2c394
Kelvin <10376375+k30v1n@users.noreply.github.com>
2021-02-20 00:03:00 -0300

    organizing files and creating docker and compose files

--- src/pubsub_create.cmd
+++
@@ -1,4 +0,0 @@
-python cloud-client/publisher.py %1 create %2
-python cloud-client/subscriber.py %1 create %2 %3
-python cloud-client/publisher.py %1 list
-python cloud-client/subscriber.py %1 list_in_topic %2
